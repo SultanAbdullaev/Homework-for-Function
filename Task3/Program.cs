3b7cc77889a7e83bcd7244b3d967abd53c1bb8f3
Sultan Abdullaev <sultan230199@yandex.ru>
2024-01-30 02:22:44 +0300

    Completed Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] array = { 1, 2, 3, 4, 5 };
+
+int[] ReversArray(int[] arr)
+{
+    int cesh = 0;
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        cesh = array[i];
+        array[i] = array[arr.Length - i - 1];
+        array[arr.Length - i - 1] = cesh;
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+foreach (int e in arr)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+
+
+ReversArray(array);
+
+PrintArray(array);
