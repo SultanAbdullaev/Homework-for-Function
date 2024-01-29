bba34e8f3e716fcb746fb9044ee90d58b18ea5c8
Sultan Abdullaev <sultan230199@yandex.ru>
2024-01-30 01:29:46 +0300

    Complated Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿int GetSumAllDigit(int a)
+{
+
+    int sum = 0;
+    while (a / 10 != 0)
+    {
+        sum += a % 10;
+        a /= 10;
+    }
+    sum += a;
+    return sum;
+}
+
+while (true)
+{
+
+    string? stop = Console.ReadLine();
+    if (stop != "q")
+    {
+        int num = Convert.ToInt32(stop);
+        if (GetSumAllDigit(num) % 2 == 0)
+        {
+            break;
+        }
+    }
+    else
+    {
+        break;
+    }
+}
